0e5ff816f42e96418ca622b56bb33495913d7094
rusmana123 <32738214+rusmana123@users.noreply.github.com>
2017-11-06 21:12:59 +0700

    Add files via upload

---
+++ A.java
@@ -0,0 +1,8 @@
+public class A{
+	public static void main(String args[]){
+	Calculator cal = new Calculator();
+	cal.pertambahan();
+	cal.pengurangan();
+}
+}
+	
---
+++ Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator{
+	int z;
+	public void pertambahan(int x, int y)
+	{
+	z = x + y;
+	System.out.println("Hasil:" + z);
+	}
+	public void pengurangan(int x, int y)
+	{
+	z = x - y;
+	System.out.println("Hasil:" + z);
+	}
+}
